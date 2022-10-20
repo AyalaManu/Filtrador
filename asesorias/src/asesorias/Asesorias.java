550d24319a451e02067314373206b585e04f3e35
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2022-10-19 19:11:56 -0500

    si

---
+++ asesorias/src/asesorias/Asesorias.java
@@ -0,0 +1,40 @@
+
+package asesorias;
+
+import javax.swing.JOptionPane;
+
+
+public class Asesorias {
+
+    
+    public static void main(String[] args) {
+        
+        
+        int contador = 0;
+        int contador2 = 0;
+        int contador3= 0;
+        
+            for (int i = 0; i < 20; i++) {
+                
+           int   num = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese los numeros") );
+          
+           if(num <0){
+               contador++;
+               
+           }else if(num>0){
+               contador2++;
+               
+           }else{
+               contador3++;
+           }
+        }
+           
+           
+            JOptionPane.showMessageDialog(null,"Los numeros negativos son: " + contador );
+            JOptionPane.showMessageDialog(null,"Los numeros positivos son: " + contador2 );
+            JOptionPane.showMessageDialog(null,"Los numeros neutros son: " + contador3 );
+        
+        
+        }
+    
+}
